67148544fbbbadc18cd0e2659e0a2f9b2acf9fb3
MTriet <minhtrietkhuu3@gmail.com>
2024-06-19 16:40:27 +0700

    update popup video + thư viện popup

--- index.js
+++ index.js
@@ -50,3 +50,22 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+//MAGNIFIC POPUP VIDEO
+$(document).ready(function () {
+  $(".popup-with-zoom-anim").magnificPopup({
+    type: "inline",
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: "auto",
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-zoom-in"
+  });
+});
